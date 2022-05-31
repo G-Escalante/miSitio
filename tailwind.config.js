2c0f4b61fb140b7553e8c9bcf94c2464e79eb6c6
Gustavo Santiago <gustavoesc12@gmail.com>
2022-05-30 21:42:41 -0500

    Commit inicial

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  darkmode: 'class',
+  content: [
+    "./public/*.html"
+  ],
+  theme: {
+    container: {
+      center: true
+    },
+    extend: {},
+    fontFamily: {
+      'ubuntu': ['Ubuntu', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
